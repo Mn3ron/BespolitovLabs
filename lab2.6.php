8fb52f62efb29fed89098444147b8736e20bc307
MneRon <daniil.bespolitov@yandex.ru>
2021-11-22 03:24:57 +0500

    add update button

--- lab2.6.php
+++ lab2.6.php
@@ -21,6 +21,8 @@
     $arr2 = task2($arr,$n,$m);
     printmass($arr2,$n,$m);
     ?>
+<p></p>
+<input type="button" value="Обновить" onClick="window.location.reload( true );">
 <p><a href= "index.php">Назад</a></p>
 </body>
 </html>
