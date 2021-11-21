215bf092bf4ec0e5cdfb0e13d122cd425dc1d938
MneRon <daniil.bespolitov@yandex.ru>
2021-11-21 23:15:25 +0500

    add lab2.1 code

--- lab2.1.php
+++ lab2.1.php
@@ -7,7 +7,36 @@
 
 <p> <p>
     <?php
-
+    print ("Треугольный массив: ");
+    For ($i = 1; $i<=10;$i++){
+        $treug[$i] = ($i*($i+1))/2;
+        print ( $treug[$i] ." ");
+    }
+    print ('<br>'. "Массив квадратов: ");
+    For ($i = 1; $i<=10;$i++){
+        $kvd[$i] = $i*$i;
+        print ( $kvd[$i] ." ");
+    }
+    $rez = array_merge($treug, $kvd);
+    print ('<br>'. "Результат: ");
+    For ($i = 0; $i<20;$i++){
+        print ( $rez[$i] ." ");
+    }
+    print ('<br>'. "Сортировка: ");
+    sort($rez);
+    For ($i = 0; $i<20;$i++){
+        print ( $rez[$i] ." ");
+    }
+    print ('<br>'. "Удален первый элемент: ");
+    unset($rez[0]);
+    For ($i = 0; $i<20;$i++){
+        print ( $rez[$i] ." ");
+    }
+    print ('<br>'. "Удалены повторяющиеся элементы: ");
+    $rez1 = array_unique($rez);
+    For ($i = 0; $i<20;$i++){
+        print ( $rez1[$i] ." ");
+    }
 
 
 
