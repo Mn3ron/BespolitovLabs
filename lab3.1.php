18741492c69ffd68212274b61a48489373e32a74
MneRon <daniil.bespolitov@yandex.ru>
2021-11-22 13:02:35 +0500

    add lab3.1 code and html

--- lab3.1.php
+++ lab3.1.php
@@ -8,13 +8,26 @@
 <p> <p>
     <?php
 
+    $a = $_GET ["a"];
+    $b = $_GET ["b"];
 
+    echo ("a = " . $a);
+    echo ("<br>");
+    echo ("b = " . $b);
+    echo ("<br>");
+    if ($a == $b)
+        echo ("Числа a и b равны");
+    elseif ($a > $b)
+        echo ("Число a > b");
+    else
+        echo ("Число b > a");
+    echo ("<hr>");
 
 
 
 
 
     ?>
-<p><a href= "index.php">Назад</a></p>
+<p><a href= "lab3.1.html">Назад</a></p>
 </body>
 </html>
